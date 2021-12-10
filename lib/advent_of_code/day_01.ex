cb7bd44f36cdef31193e58b536a0adc8a1fd0fe4
Maciej Krzywda <maciej.krzywdaa@gmail.com>
2021-12-10 18:35:47 +0100

    Day 1: Report Repair

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,30 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
-  end
 
-  def part2(args) do
+    def part1(args) do
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> combine(2)
+      |> Enum.find(&(Enum.sum(&1) == 2020))
+      |> List.flatten()
+      |> Enum.reduce(1, &(&2 * &1))
+    end
+
+    def combine(input, len)
+    def combine(_, 0), do: [[]]
+    def combine([], _), do: []
+
+    def combine([h | t], len),
+      do: Enum.map(combine(t, len - 1), &[h | &1]) ++ combine(t, len)
+
+
+    def part2(args) do
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> combine(3)
+      |> Enum.find(&(Enum.sum(&1) == 2020))
+      |> List.flatten()
+      |> Enum.reduce(1, &(&2 * &1))
+    end
   end
-end
